75646b7a272e848e43929055679b1b6e0dfb4851
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-07-27 11:41:23 +0200

    SEO

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="logo-container">
-        <span class="logo-title">"picture"</span>
+        <h1 class="logo-title">"picture"</h1>
         <i class="logo-by">by</i>
         <img src="~/assets/images/svg/logo.svg" width="466" alt="SENSE" class="logo-logo" />
     </div>
@@ -17,6 +17,8 @@
 }
 .logo-title {
     font-size: 3em;
+    font-weight: 400;
+    margin: 0;
 }
 .logo-by {
     font-size: 2em;
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+var hostname = process.env.HOSTNAME;
+var description = '';
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -16,14 +19,38 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: description },
+      { name: 'keywords', content: 'sense,artists,music,release,club,labels,services,picture,dashboard' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'category', content: 'music' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'theme-color', content: '#1d1d1d' },
       { name: 'msapplication-TileColor', content: '#1d1d1d' },
-      { name: 'theme-color', content: '#1d1d1d' }
+      { name: 'msapplication-TileImage', content: '/favicon.png' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-title', content: 'picture - SENSE' },
+      { name: 'og:title', content: 'picture - SENSE' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'https://'+hostname },
+      { name: 'og:image', content: 'https://'+hostname+'/favicon.png' },
+      { name: 'og:description', content: description },
+      { name: 'og:site_name', content: 'picture - SENSE' },
+      { name: 'og:locale', content: 'en_US' },
+      { name: 'og:locale:alternate', content: 'en_US' },
+      { name: 'twitter:creator', content: '@senseartists' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@senseartists' },
+      { name: 'twitter:title', content: 'picture by SENSE' },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image', content: 'https://'+hostname+'/favicon.png' },
+      { name: 'twitter:image:alt', content: 'SENSE picture' },
+      { name: 'application:name', content: 'picture - SENSE' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' }
+      { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
+      { rel: 'canonical', href: 'https://'+hostname }
     ]
   },
 
@@ -49,7 +76,15 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/sitemap'
   ],
+
+  // Configuration du module sitemap
+  sitemap: {
+    hostname: 'https://senseartists.club',
+    gzip: true,
+    routes : ['/']
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
